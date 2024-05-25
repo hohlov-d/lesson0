17c676e9972696a1e3697caf0ec7a0539101596a
hohlov-d <hohlov_d.o@mail.ru>
2024-05-26 01:26:02 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+a = 'Зайку бросила хозяйка'
+print(len(a)) # Задача Длина слова
+
+first = 5
+second = 5
+summa = first + second
+diff = first - second
+print(summa,diff) # Задача Суммы и Разности
+
+import statistics
+numbers = [10,15,20]
+average = statistics.mean(numbers)
+print(average) # Задача Среднее арифметическое
+
+first_string = 'Вторник'
+second_string = 'Понедельник, '
+print(second_string + first_string) # Задача Простые строки
+
+a = 5
+b = 3
+c = 2
+f = ((((a * b) + (a * c)) ** 3) / 2)
+print(f) # Задача Сложная формула
